576a6d5ab332e38a74333926be00e46848855fbe
Mauricio Vieira <mauricio@mauriciovieira.net>
2020-06-14 19:59:14 -0300

    Create project with bundle gem

---
+++ lib/moteefe_delivery.rb
@@ -0,0 +1,6 @@
+require "moteefe_delivery/version"
+
+module MoteefeDelivery
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/moteefe_delivery_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe MoteefeDelivery do
+  it "has a version number" do
+    expect(MoteefeDelivery::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
